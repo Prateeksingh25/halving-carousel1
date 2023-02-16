99daad5543cf2a01c38fce32aa18a1bc98fa0ff3
Prateek Singh <94031538+Prateeksingh25@users.noreply.github.com>
2023-02-16 11:42:23 +0530

    Add files via upload

---
+++ DecrementingCarousel.java
@@ -0,0 +1,40 @@
+package com.epam.rd.autotasks;
+
+import java.util.ArrayList;
+
+public class DecrementingCarousel {
+    protected final int capacity;
+    static int[] carousel;
+    int index;
+    boolean isRun;
+
+    {
+        index = 0;
+        isRun = false;
+    }
+
+    public DecrementingCarousel(int capacity) {
+        this.capacity = capacity;
+        carousel = new int[capacity];
+
+    }
+
+    public boolean addElement(int element){
+
+        if (element > 0 && index < capacity && !isRun) {
+            carousel[index++] = element;
+            return true;
+        }
+        return false;
+        //throw new UnsupportedOperationException();
+    }
+
+    public CarouselRun run(){
+        if (!isRun) {
+            isRun = true;
+            return new CarouselRun();
+        }
+        return null;
+        //throw new UnsupportedOperationException();
+    }
+}
